5e0b0c675f65214725c33a35e84fe10b93147a9e
jerry3027 <zhangju0707@gmail.com>
2022-01-26 23:44:29 -0500

    Getting Started

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Jerry Zhang on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
